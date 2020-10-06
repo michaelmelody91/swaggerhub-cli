f53c43ca5ba6050d9f313e2ebe357107a87212c7
michaelmelody91 <michaelmelody91@gmail.com>
2020-10-06 14:35:08 +0100

    21

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-output=$(/usr/src/cli/bin/run $*)
-output=$(true); status=$?;
+output=$(/usr/src/cli/bin/run $*); status=$?;
 echo "$output"
 echo "status = $status"
 echo "::set-output name=output::$output"
