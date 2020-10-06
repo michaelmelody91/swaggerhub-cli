6ef4d65d3b43b201f6a91b9edf035f2ed904266c
michaelmelody91 <michaelmelody91@gmail.com>
2020-10-06 16:47:23 +0100

    1.0.22

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 output=$(/usr/src/cli/bin/run $*); status=$?;
-echo "$output"
-echo "status = $status"
 echo "::set-output name=output::$output"
 exit $status
--- .github/action/pre-action.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp -r ../../bin ./
-cp -r ../../src ./
-cp ../../package*.json ./
