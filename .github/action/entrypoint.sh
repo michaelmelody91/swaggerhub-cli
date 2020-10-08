a2a4d7930d32fa09300bafce5cd74bd7f03a9f8a
michaelmelody91 <michaelmelody91@gmail.com>
2020-10-08 16:35:47 +0100

    checking output to variable in entrypoint

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 output=$(/cli/bin/run $*); status=$?;
 echo "output is to be set to $output"
+echo "\nnew line to verify output variable\n"
 echo "::set-output name=response::$output"
 exit $status
